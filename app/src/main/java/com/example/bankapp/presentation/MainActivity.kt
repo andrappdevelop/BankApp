c51995a6dd27a4638ac387b0d6d2a0c79f2f6761
IvanUsatov <andrappdevelop@gmail.com>
2023-03-18 12:24:13 +0700

    add layout

--- app/src/main/java/com/example/bankapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/bankapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.bankapp
+package com.example.bankapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bankapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
